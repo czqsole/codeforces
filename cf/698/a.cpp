5150aabbc9f65e7c8c0f3f80c9052dec27c57d3b
czq-win <czqsole@gmail.com>
2021-02-15 20:47:29 +0800

    '698-a'

--- cf/698/a.cpp
+++ cf/698/a.cpp
@@ -10,15 +10,43 @@ const int N = 100005;
 int main() {
     int t, n;
     long long d[N];
+    long long dd[N];
+    long long c[N];
     cin >> t;
     while(t--) {
         cin >> n;
-        for (int i = 0; i < n; ++i) {
+        for (int i = 0; i < 2*n; ++i) {
             cin >> d[i];
         }
-        sort(d, d + n);
-        int ans = 0;
-
-
+        sort(d, d + 2*n);
+        int ans = 1;
+        for (int i = 0; i < n; ++i) {
+            if(d[2*i] != d[2*i + 1]) {
+                ans = 0;
+                break;
+            }
+            dd[i] = d[2*i];
+        }
+        if(!ans) {
+            cout << "NO\n";
+        }
+        for(int i=0; i < n-1;i++) {
+            if((dd[i] == dd[i+1]) || (dd[i+1] - dd[i]) % (2*(i + 1))) {
+                ans = 0;
+                break;
+            }
+            c[i] = (dd[i+1] - dd[i])/2/(i + 1);
+        }
+        for (int i = 1; i < n; ++i) {
+            dd[0] -= 2*(i+1)* c[i];
+        }
+        if(dd[0] <= 0 || dd[0] % (2*n)) {
+            ans = 0;
+        }
+        if(ans) {
+            cout << "YES\n;"
+        } else {
+            cout << "NO\n";
+        }
     }
 }
